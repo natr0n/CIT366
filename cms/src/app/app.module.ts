2c64b8ba6b695c6996fa93d2ad18a2f05d3de81a
Nate Thorne <tho12101@byui.edu>
2018-09-26 17:51:11 -0600

    Added bootstrap to main project

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactsDetailComponent } from './contacts/contacts-detail/contacts-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactListComponent,
+    ContactsDetailComponent
   ],
   imports: [
     BrowserModule
