5daa5ea06c614df35f2819d8ace7868a5917066a
Nate Thorne <tho12101@byui.edu>
2018-10-08 19:53:26 -0600

    Lesson 3

--- cms/src/app/messages/message-edit/message-edit.component.ts
+++ cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  public currentSender: string = "Nathan Thorne";
+  @ViewChild('subject') subject: ElementRef;
+  @ViewChild('msgText') msgText: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
+
+
+  onSendMessage() {
+    const subject = this.subject.nativeElement.value;
+    const msgText = this.msgText.nativeElement.value;
+    const newMessage = new Message(4, subject, msgText, this.currentSender);
+    this.addMessageEvent.emit(newMessage);
+    this.onClear();
+  }
+
+  onClear() {
+    this.subject.nativeElement.value = ' ';
+    this.msgText.nativeElement.value = ' ';
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
+
+ 
 
 }
--- cms/src/app/messages/message-list/message-list.component.ts
+++ cms/src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
+messages: Message[] = [
+  new Message(1, "Homework", "Can you help me with all my assignments?", "Nathan Thorne"),
+  new Message(2, "Homework", "Well sure, but why don't we just start with lesson 3.", "Bro. Thane"),
+  new Message(3, "Extra Credit", "Sweet Beans! Can I have an A+ just for fun as well?", "Nathan Thorne")
 
+]
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddMessage(message: Message){
+    this.messages.push(message);
   }
 
 }
